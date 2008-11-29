862f9f1e07656081d3528c7696a38e323a5b4476
timofei1394 <timofei1394@63525bb8-ba65-11dd-aa87-4b319d2fb4de>
2008-11-29 12:16:49 +0000

    Some small fix in Y/widget/widget.c

--- trunk/Y/setup.h
+++ trunk/Y/setup.h
@@ -41,19 +41,19 @@
 #define PACKAGE "Y"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "markbt@efaref.net"
+#define PACKAGE_BUGREPORT "neochapay@gmail.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "Y"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Y 0.2.1"
+#define PACKAGE_STRING "Y 0.2.7"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "Y"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.2.1"
+#define PACKAGE_VERSION "0.2.7"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -63,7 +63,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.2.1"
+#define VERSION "0.2.7"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
